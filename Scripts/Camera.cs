321e00fba6d443570c770265ccc5e516111837a7
Tyler Beaupre <tylerbeaupre4@gmail.com>
2020-12-14 14:10:57 -0500

    Adds camera tracking

---
+++ Scripts/Camera.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+	public Boid[] boids;
+
+    // Start is called before the first frame update
+    void Start()
+	{
+		boids = FindObjectsOfType<Boid>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		Vector3 perceivedCenter = Vector3.zero;
+		int count = 0;
+		foreach(Boid boid in boids) {
+			if (boid != null) {
+				count++;
+				perceivedCenter += boid.gameObject.transform.position;
+			}
+		}
+
+		if (count != 0) {
+			perceivedCenter = perceivedCenter / count;
+			Vector3 displacement = perceivedCenter - transform.position;
+			transform.forward = displacement;
+		}
+    }
+}
