c057b68127d75b1e2395b6d49b8dcc632b21a0ed
Tyler Beaupre <tylerbeaupre4@gmail.com>
2020-12-30 11:59:15 -0500

    Adds ShipSelectionScene and ship selection UI

---
+++ Scripts/UI/ShipAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShipAttribute : MonoBehaviour
+{
+	public string symbol;
+	public float value;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		value = Random.value;
+		ShipAttributePoint[] points = GetComponentsInChildren<ShipAttributePoint>();
+		Debug.Log(points.Length);
+
+		for (int i = 0; i < points.Length; i++) {
+			points[i].gameObject.SetActive(value >= (i + 1f) / 10f);
+		}
+
+		Text symbolText = GetComponentInChildren<Text>();
+		symbolText.text = symbol;
+	}
+}
