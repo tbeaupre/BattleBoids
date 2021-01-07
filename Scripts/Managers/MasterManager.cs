0eb1ab1faeb6a2c2c4bdc0434fc5ae0121ae9cd9
Tyler Beaupre <tylerbeaupre4@gmail.com>
2021-01-07 16:48:02 -0500

    Adds MasterScene and MainMenuScene

---
+++ Scripts/Managers/MasterManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MasterManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+	{
+		SceneManager.LoadSceneAsync("MainMenuScene", LoadSceneMode.Additive);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Scripts/UI/PlayButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayButton : MonoBehaviour
+{
+	public void Play()
+	{
+		SceneManager.LoadSceneAsync("ShipSelectionScene", LoadSceneMode.Additive);
+		SceneManager.UnloadSceneAsync("MainMenuScene");
+	}
+}
