bd712f444e250c2a224b9804c3b979cb640fcf9d
Tyler Beaupre <tylerbeaupre4@gmail.com>
2021-01-12 11:13:12 -0500

    Adds PilotInfoPanel

---
+++ Scripts/UI/PilotSelection/PilotAttribute.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PilotAttribute : MonoBehaviour
+{
+	public string symbol;
+	public float value = 0;
+	AttributePoint[] points;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		points = GetComponentsInChildren<AttributePoint>();
+
+		for (int i = 0; i < points.Length; i++) {
+			points[i].gameObject.GetComponent<Image>().enabled = value >= (i + 1f) / 10f;
+		}
+
+		Text symbolText = GetComponentInChildren<Text>();
+		symbolText.text = symbol;
+	}
+
+	public void SetValue(float newValue) {
+		value = newValue;
+
+		for (int i = 0; i < points.Length; i++) {
+			points[i].gameObject.GetComponent<Image>().enabled = value >= (i + 1f) / 10f;
+		}
+
+	}
+}
