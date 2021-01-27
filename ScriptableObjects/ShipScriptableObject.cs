5fefe3362231d169b565e0be19ec3b546e6cb491
Tyler Beaupre <tbeaupre@wpi.edu>
2021-01-27 15:42:15 -0500

    Adds ShipScriptableObject

---
+++ ScriptableObjects/ShipScriptableObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Ship", menuName = "ScriptableObjects/ShipScriptableObject", order = 1)]
+public class ShipScriptableObject : ScriptableObject
+{
+    public string shipName;
+	public Sprite portrait;
+
+	public float sensors;
+	public float acceleration;
+	public float topSpeed;
+	public float armor;
+	public float range;
+	public float damage;
+}
