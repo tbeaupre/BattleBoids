36f97be892bffa8483507c2a54149f16c5d93c54
Tyler Beaupre <tbeaupre@wpi.edu>
2021-01-27 15:58:23 -0500

    Adds PilotScriptableObject

---
+++ ScriptableObjects/PilotScriptableObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Pilot", menuName = "ScriptableObjects/PilotScriptableObject", order = 1)]
+public class PilotScriptableObject : ScriptableObject
+{
+    public string pilotName;
+    public Sprite portrait;
+
+    public float sociability;
+	public float ego;
+	public float persistence;
+	public float vision;
+	public float skill;
+}
