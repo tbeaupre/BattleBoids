6db831a4ef6f70ece8d4a1aca9e739585bd2faaf
Tyler Beaupre <tbeaupre@wpi.edu>
2021-01-29 11:20:10 -0500

    Adds EnemyFactory

---
+++ Scripts/Factories/EnemyFactory.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class EnemyFactory
+{
+    public static void InstantiateTeam(EnemyTeamScriptableObject enemyTeam) 
+    {
+        for (int i = 0; i < enemyTeam.ships.Length; i++) {
+            GameObject enemy = Object.Instantiate(
+                enemyTeam.ships[i].prefab,
+                enemyTeam.spawnPoints.spawnPoints[i],
+                Quaternion.identity
+            );
+            enemy.GetComponent<MeshRenderer>().material = Resources.Load<Material>("EnemyMaterial");
+            Boid boid = enemy.GetComponent<Boid>();
+            boid.isEnemy = true;
+            boid.Initialize(enemyTeam.pilots[i], enemyTeam.ships[i]);
+        }
+    }
+}
