e586863b5b68c9b64f7e962817c81e8579fc9f47
Tyler Beaupre <tbeaupre@wpi.edu>
2021-02-12 16:58:36 -0500

    Selects a boid when the info panel is selected and shows icon

---
+++ Scripts/UI/Battle/BoidInfoPanel/SelectTargetOnClick.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectTargetOnClick : MonoBehaviour
+{
+    public SelectedBoidScriptableObject HoveredBoid;
+    public SelectedBoidScriptableObject TargetBoid;
+
+    public void OnMouseDown()
+    {
+        TargetBoid.Boid = TargetBoid.Boid == HoveredBoid.Boid ? null : HoveredBoid.Boid;
+    }
+}
---
+++ Scripts/UI/Battle/BoidInfoPanel/ShowIconWhenSelected.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowIconWhenSelected : MonoBehaviour
+{
+    public SelectedBoidScriptableObject TargetBoid;
+    public Image IconImage;
+    public BoidInfoPanel AssociatedBoidInfoPanel;
+
+    // Update is called once per frame
+    void Update()
+    {
+        IconImage.enabled = TargetBoid.Boid != null && AssociatedBoidInfoPanel.boid == TargetBoid.Boid;
+    }
+}
